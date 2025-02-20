505bcbb37eebdf6f7561bced3cf9afa5886d09fe
2xxbin <2xxbin1208@gmail.com>
2025-02-21 02:30:42 +0900

    Add Character Sample Audio

---
+++ customAudioPlayer.js
@@ -0,0 +1,47 @@
+const players = document.querySelectorAll('.audio-container__player');
+
+players.forEach((player) => {
+  /** @type {HTMLButtonElement} */
+  const button = player.querySelector('button');
+  /** @type {HTMLImageElement} */
+  const icon = button.querySelector('img');
+  /** @type {HTMLAudioElement} */
+  const audio = player.querySelector('audio');
+
+  let isPlaying = false;
+
+  function toggle() {
+    if (isPlaying) {
+      audio.pause();
+      audio.currentTime = 0;
+      icon.src = './ui 에셋/play-circle.svg';
+    } else {
+      // 현재 재생 중인 모든 오디오를 멈추기
+      players.forEach((otherPlayer) => {
+        const otherAudio = otherPlayer.querySelector('audio');
+        const otherIcon = otherPlayer.querySelector('img');
+        if (otherAudio !== audio) {
+          otherAudio.pause();
+          otherAudio.currentTime = 0;
+          otherIcon.src = './ui 에셋/play-circle.svg';
+          otherPlayer.dataset.playing = 'false';
+        }
+      });
+
+      audio.volume = 1.0;
+      audio.play();
+      icon.src = './ui 에셋/stop-circle.svg';
+    }
+    isPlaying = !isPlaying;
+    player.dataset.playing = isPlaying ? 'true' : 'false';
+  }
+
+  function audioEnd() {
+    isPlaying = false;
+    icon.src = './ui 에셋/play-circle.svg';
+    player.dataset.playing = 'false';
+  }
+
+  button.addEventListener('click', toggle);
+  audio.addEventListener('ended', audioEnd);
+});
